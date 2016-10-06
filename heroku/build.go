7e2c8f3010cce9384b9257447f1a7ae90ee42789
Dirk Gadsden <dirk@esherido.com>
2016-10-06 14:01:45 -0700

    Poll for build status when performing Heroku deployments

--- heroku/build.go
+++ heroku/build.go
@@ -3,8 +3,15 @@ package heroku
 import (
 	"encoding/json"
 	"fmt"
+	"io/ioutil"
 	"net/http"
 )
+
+type Build struct {
+	Id         string      `json:"id"`
+	SourceBlob *SourceBlob `json:"source_blob"`
+	Status string `json:"status"`
+}
 
 type SourceBlob struct {
 	Checksum string `json:"checksum,omitempty"`
@@ -12,14 +19,55 @@ type SourceBlob struct {
 	Version  string `json:"url"`
 }
 
-func (c *Client) BuildCreate(appId string, sourceBlob *SourceBlob) (*http.Response, error) {
+func (c *Client) BuildCreate(appId string, sourceBlob *SourceBlob) (*Build, *http.Response, error) {
 	body, err := json.Marshal(map[string]interface{}{
 		"source_blob": sourceBlob,
 	})
 	if err != nil {
-		return nil, err
+		return nil, nil, err
 	}
 
 	url := c.BaseUrl() + fmt.Sprintf("/apps/%v/builds", appId)
-	return c.MakeRequest("POST", url, &body)
+	resp, err := c.MakeRequest("POST", url, &body)
+	if err != nil {
+		return nil, resp, err
+	}
+
+	var build Build
+	err = c.readResponseInto(resp, &build)
+	if err != nil {
+		return nil, resp, err
+	}
+
+	return &build, resp, nil
+}
+
+func (c *Client) BuildInfo(appId string, id string) (*Build, *http.Response, error) {
+	url := c.BaseUrl() + fmt.Sprintf("/apps/%v/builds/%v", appId, id)
+	resp, err := c.MakeRequest("GET", url, nil)
+	if err != nil {
+		return nil, resp, err
+	}
+
+	var build Build
+	err = c.readResponseInto(resp, &build)
+	if err != nil {
+		return nil, resp, err
+	}
+
+	return &build, resp, nil
+}
+
+func (c *Client) readResponseInto(resp *http.Response, val interface{}) error {
+	body, err := ioutil.ReadAll(resp.Body)
+	if err != nil {
+		return err
+	}
+
+	err = json.Unmarshal(body, val)
+	if err != nil {
+		return err
+	}
+
+	return nil
 }
--- phases/heroku.go
+++ phases/heroku.go
@@ -3,6 +3,7 @@ package phases
 import (
     "fmt"
     "net/http"
+    "time"
 
     "github.com/Everlane/evan/common"
     "github.com/Everlane/evan/heroku"
@@ -18,31 +19,59 @@ func (hbp *HerokuBuildPhase) CanPreload() bool {
 }
 
 func (hbp *HerokuBuildPhase) Execute(deployment common.Deployment) error {
-    err := hbp.createBuild(deployment)
+    build, err := hbp.createBuild(deployment)
     if err != nil {
         return err
     }
 
+    build, err = hbp.PollBuildStatus(build)
+    if err != nil {
+        return err
+    }
+    if build.Status == "failed" {
+        return fmt.Errorf("Build %v failed", build.Id)
+    }
+
+    // Heroku will have automatically deployed that new build as a release, so
+    // we can consider this phase done.
     return nil
 }
 
-func (hbp *HerokuBuildPhase) createBuild(deployment common.Deployment) error {
+func (hbp *HerokuBuildPhase) createBuild(deployment common.Deployment) (*heroku.Build, error) {
     githubRepo := common.NewGithubRepositoryFromDeployment(deployment)
     tarballUrl, err := githubRepo.GetArchiveLink(common.Tarball)
     if err != nil {
-        return err
+        return nil, err
     }
 
-    resp, err := hbp.Client.BuildCreate(hbp.AppId, &heroku.SourceBlob{
+    build, resp, err := hbp.Client.BuildCreate(hbp.AppId, &heroku.SourceBlob{
         Url: tarballUrl,
         Version: deployment.Ref(),
     })
     if err != nil {
-        return err
+        return nil, err
     }
     if resp.StatusCode != http.StatusCreated {
-        return fmt.Errorf("Non-creation status code (expected %v, got %v)", http.StatusCreated, resp.StatusCode)
+        return nil, fmt.Errorf("Non-creation status code (expected %v, got %v)", http.StatusCreated, resp.StatusCode)
     }
 
-    return nil
+    return build, nil
+}
+
+func (hbp *HerokuBuildPhase) PollBuildStatus(sourceBuild *heroku.Build) (*heroku.Build, error) {
+    for true {
+        build, _, err := hbp.Client.BuildInfo(hbp.AppId, sourceBuild.Id)
+        if err != nil {
+            return nil, err
+        }
+
+        if build.Status == "pending" {
+            time.Sleep(10 * time.Second)
+            continue
+        }
+
+        return build, nil
+    }
+
+    panic("Unreachable!")
 }
