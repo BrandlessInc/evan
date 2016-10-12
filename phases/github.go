46861a15ae376559458fb9bdf6497978065c8962
Dirk Gadsden <dirk@esherido.com>
2016-10-11 18:53:01 -0700

    Add GitHub status checks phase

---
+++ phases/github.go
@@ -0,0 +1,65 @@
+package phases
+
+import (
+	"fmt"
+	"time"
+
+	"github.com/Everlane/evan/common"
+)
+
+type GithubCombinedStatusPhase struct {
+	// Treat no status checks as okay (ignores status reported by GitHub)
+	AllowEmpty bool
+	// How long to wait for the combined status to no longer be pending
+	Timeout time.Duration
+}
+
+func (gh *GithubCombinedStatusPhase) CanPreload() bool {
+	return false
+}
+
+func (gh *GithubCombinedStatusPhase) Execute(deployment common.Deployment, data interface{}) error {
+	repo := deployment.Application().Repository()
+	ref := deployment.SHA1()
+	if ref == "" {
+		ref = deployment.Ref()
+	}
+
+	client, err := common.GithubClient(deployment)
+	if err != nil {
+		return err
+	}
+
+	start := time.Now()
+
+	for true {
+		elapsed := time.Since(start)
+		if elapsed > gh.Timeout {
+			return fmt.Errorf("Timed out waiting for combined status")
+		}
+
+		status, _, err := client.Repositories.GetCombinedStatus(repo.Owner(), repo.Name(), ref, nil)
+		if err != nil {
+			return err
+		}
+
+		// Skip if there are no status checks is allowed
+		if *status.TotalCount == 0 && gh.AllowEmpty {
+			return nil
+		}
+
+		state := *status.State
+		switch state {
+		case "pending":
+			continue
+		case "success":
+			return nil
+		case "failure":
+			return fmt.Errorf("Combined status is failure")
+		default:
+			return fmt.Errorf("Unknown combined status: %v", state)
+		}
+	}
+
+	panic("Unreachable")
+}
