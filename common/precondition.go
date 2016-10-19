a2b396558c2643d82e869bf73c919744b413cace
Dirk Gadsden <dirk@esherido.com>
2016-10-18 17:47:12 -0700

    Allow preconditions to return error signifying status pending

--- common/precondition.go
+++ common/precondition.go
@@ -3,3 +3,13 @@ package common
 type Precondition interface {
 	Status(Deployment) error
 }
+
+// Wraps an error to signify that it's not a true error but rather pending
+// resolution.
+type PendingError struct {
+	error
+}
+
+func NewPendingError(err error) *PendingError {
+	return &PendingError{err}
+}
